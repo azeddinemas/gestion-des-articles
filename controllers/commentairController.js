5b46fe607578dcdf4fe4ec8a98c270fe45129246
azeddine <maslouhazeddine@gmail.com>
2022-10-06 14:14:47 +0100

    azoz

---
+++ controllers/commentairController.js
@@ -0,0 +1,29 @@
+const db = require('../models/commentairModel');
+const express = require('express');
+const commentair = require('../models/commentairModel');
+const app = express();
+
+
+const Addcommentair = (req, res) => {
+    const { body } = req;
+    commentair.create({...body })
+        .then(() => { res.json({ msg: "commentair added success" }) })
+        .catch(() => { res.json({ msg: 'error' }) })
+}
+
+const getall = (req, res) => {
+    commentair.findAll().then(comments => {
+        res.json({ comments })
+    })
+}
+
+const deletecom = (req, res) => {
+    const { id } = req.body;
+    commentair.destroy({ where: { id: id } }).then(() => {
+        res.json({ msg: 'deleted' })
+    }).catch(() => {
+        res.json({ msg: 'not deleted' })
+    })
+}
+
+module.exports = { Addcommentair, getall, deletecom }
