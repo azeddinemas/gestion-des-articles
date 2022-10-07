2b58482390ed580abc8e296ea83035d1404c416d
zerzkhane <zezkhanemourad@gmail.com>
2022-10-07 15:22:41 +0100

    add new function to CategorieController

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -11,14 +11,16 @@ const route = Router()
 route.post('/addpost', post.AddPost );
 route.get('/', home.index );
 route.get('/dashboard', home.dashboard );
-route.get('/categories', home.categories );
-route.get('/articles', home.articles );
-route.get('/getAllcategorie', categorie.getAllCategorie );
-route.post('/addcategorie', categorie.AddCategorie );
 
+route.get('/articles', home.articles );
 route.get('/articles', post.GetAllPost );
 route.get('/addpost', home.addPost );
 
 
+// route.get('/getAllcategorie', categorie.getAllCategorie );
+route.post('/categories', categorie.AddCategorie );
+route.get('/categories', categorie.getAllCategorie );
+route.post('/categories/:id', categorie.deletecategorie)
+route.post('/categorie/:id', categorie.getonecategorie)
 
 module.exports = route
